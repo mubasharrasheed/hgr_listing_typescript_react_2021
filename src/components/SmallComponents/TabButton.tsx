b2fea73fb2aa55de24b7b1b35f7faf9329f1fbab
zohaib ali shah <zohaibalishah1997@gmail.com>
2021-08-02 14:28:10 +0500

    tabs onclick

--- src/components/SmallComponents/TabButton.tsx
+++ src/components/SmallComponents/TabButton.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 
 export default function TabButton(props: any) {
-  const { title, active } = props;
+  const { title, active, onChangeTab, index } = props;
   return (
     <div className="col pl-lg-0 my-2 my-md-0 lh-1">
       <button
+        onClick={() => onChangeTab(index)}
         className={`btn px-lg-3 px-2 ${
-          active ? "bg-purple text-white" : "bg-trans text-dark"
+          active == index ? "bg-purple text-white" : "bg-trans text-dark"
         } border-0 br-8 py-1`}
       >
         {title}
--- src/components/SmallComponents/TabsList.tsx
+++ src/components/SmallComponents/TabsList.tsx
@@ -1,13 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import TabButton from "./TabButton";
 export default function TabsList() {
+  const [active, setActive] = useState(1);
+  const onChangeTab = (value: any) => {
+    setActive(value);
+  };
   return (
     <div className="row mx-auto my-3 d-blue align-items-center bg-white br-8 p-2">
-      <TabButton active={true} title={"Active listings"} />
-      <TabButton title={"Pending listings"} />
-      <TabButton title={"Active listings"} />
-      <TabButton title={"Terminated listings"} />
-      <TabButton title={"Imported listings"} />
+      <TabButton
+        onChangeTab={onChangeTab}
+        index={1}
+        active={active}
+        title={"Active listings"}
+      />
+      <TabButton
+        onChangeTab={onChangeTab}
+        index={2}
+        active={active}
+        title={"Pending listings"}
+      />
+      <TabButton
+        onChangeTab={onChangeTab}
+        index={3}
+        active={active}
+        title={"Active listings"}
+      />
+      <TabButton
+        onChangeTab={onChangeTab}
+        index={4}
+        active={active}
+        title={"Terminated listings"}
+      />
+      <TabButton
+        onChangeTab={onChangeTab}
+        index={5}
+        active={active}
+        title={"Imported listings"}
+      />
     </div>
   );
 }

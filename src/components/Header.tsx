0c50047b79b75755ed97196b8e42356d6d04809b
zohaib ali shah <zohaibalishah1997@gmail.com>
2021-08-02 13:43:17 +0500

    drop down

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,11 +3,10 @@ import ring_icon from "../assets/notification.svg";
 import flag from "../assets/flag-round-500.svg";
 import amazon from "../assets/amazon-icon-1.svg";
 import { Menu, Dropdown, Button, Space } from "antd";
-import plus from "../assets/plus.svg";
-import { Progress } from "antd";
+import DropDownMenu from './SmallComponents/DropDownMenu'
+
 import TabsList from "./SmallComponents/TabsList";
 import SearchBar from "./SmallComponents/SearchBar";
-import HeaderDropDownItem from "./SmallComponents/HeaderDropDownItem";
 import { AudioOutlined } from "@ant-design/icons";
 function Header(props: any) {
   const suffix = (
@@ -18,48 +17,7 @@ function Header(props: any) {
       }}
     />
   );
-  const menu = (
-    <Menu>
-      <div className="drop-down">
-        <div className="row mx-auto my-2 align-items-center">
-          <div className="col-12">
-            <Progress
-              percent={40}
-              showInfo={false}
-              strokeColor={{
-                "0%": "#87d068",
-                "100%": "#87d068",
-              }}
-            />
-          </div>
-          <div className="col-md-12">
-            <div className="d-flex my-1">
-              <span className="d-blue small">Quota:45%(12/30)</span>
-              <span className="text-success ml-auto small">Upgrade</span>
-            </div>
-          </div>
-          <HeaderDropDownItem />
-          <HeaderDropDownItem />
-
-          <div className="col-md-12 mb-2 pl-2">
-            <div className="bg-lighter br-15 py-1 w-fit d-flex align-items-center">
-              <div className="d-blue lh-1 pl-1 font-weight-bold px-2 small">
-                Add new channel
-              </div>
-              <Button className="border-0 bg-trans pl-2 pr-1 py-0 lh-1 h-100">
-                <img src={plus} height="18" alt="" />
-              </Button>
-            </div>
-          </div>
-        </div>
-      </div>
-      <Menu.Item className="p-0 bg-trans">
-        <div className="d-flex py-2 pr-2 pr-lg-4 mx-auto text-right text-danger align-items-center small">
-          <i className="fas fa-sign-out-alt pr-1 ml-auto"></i> Logout
-        </div>
-      </Menu.Item>
-    </Menu>
-  );
+  
   return (
     <div>
       <div className="bg-trans" style={{ padding: 0 }}>
@@ -85,7 +43,7 @@ function Header(props: any) {
                 <div className="ml-auto">
                   <Space direction="vertical">
                     <Space wrap>
-                      <Dropdown overlay={menu} placement="bottomLeft">
+                      <Dropdown overlay={<DropDownMenu />} placement="bottomLeft">
                         <Button>
                           <i className="fas fa-ellipsis-v"></i>
                         </Button>
---
+++ src/components/SmallComponents/DropDownMenu.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { Menu, Dropdown, Button, Space } from "antd";
+import { Progress } from "antd";
+import HeaderDropDownItem from "../SmallComponents/HeaderDropDownItem";
+import plus from "../../assets/plus.svg";
+
+export default function DropDownMenu() {
+  return (
+    <Menu>
+      <div className="drop-down">
+        <div className="row mx-auto my-2 align-items-center">
+          <div className="col-12">
+            <Progress
+              percent={40}
+              showInfo={false}
+              strokeColor={{
+                "0%": "#87d068",
+                "100%": "#87d068",
+              }}
+            />
+          </div>
+          <div className="col-md-12">
+            <div className="d-flex my-1">
+              <span className="d-blue small">Quota:45%(12/30)</span>
+              <span className="text-success ml-auto small">Upgrade</span>
+            </div>
+          </div>
+          <HeaderDropDownItem />
+          <HeaderDropDownItem />
+
+          <div className="col-md-12 mb-2 pl-2">
+            <div className="bg-lighter br-15 py-1 w-fit d-flex align-items-center">
+              <div className="d-blue lh-1 pl-1 font-weight-bold px-2 small">
+                Add new channel
+              </div>
+              <Button className="border-0 bg-trans pl-2 pr-1 py-0 lh-1 h-100">
+                <img src={plus} height="18" alt="" />
+              </Button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Menu.Item className="p-0 bg-trans">
+        <div className="d-flex py-2 pr-2 pr-lg-4 mx-auto text-right text-danger align-items-center small">
+          <i className="fas fa-sign-out-alt pr-1 ml-auto"></i> Logout
+        </div>
+      </Menu.Item>
+    </Menu>
+  );
+}

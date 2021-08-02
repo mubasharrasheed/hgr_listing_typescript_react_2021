00ce8e76fb5a9dd6d43cdcb54ff9fde5e0fa7cec
mehrozasif <mehroz.rj@gmail.com>
2021-08-02 15:22:57 +0500

    latest

--- src/components/ToggleButton.tsx
+++ src/components/ToggleButton.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 
-export default function ToggleButton({ toggle }) {
+export default function ToggleButton(props: any) {
   return (
     <i
-      onClick={toggle}
+      onClick={props.toggle}
       className="fas fa-chevron-right text-white bg-dark rounded p-1 m-2 w-fit"
     ></i>
   );

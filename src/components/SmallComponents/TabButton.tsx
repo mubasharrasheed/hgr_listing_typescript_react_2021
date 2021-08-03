59a430449b81e9e28d5ae08d4b53f7da7456a7ae
zohaib ali shah <zohaibalishah1997@gmail.com>
2021-08-03 13:58:01 +0500

    desfine types

--- src/components/SmallComponents/TabButton.tsx
+++ src/components/SmallComponents/TabButton.tsx
@@ -3,14 +3,14 @@ interface Props {
   title: string;
   active: number;
   index: number;
-  onChangeTab: () => number;
+  onChangeTab: () => void;
 }
 export default function TabButton(props: Props) {
   const { title, active, onChangeTab, index } = props;
   return (
     <div className="col pl-lg-0 my-2 my-md-0 lh-1">
       <button
-        onClick={() => onChangeTab(index)}
+        onClick={onChangeTab}
         className={`btn px-lg-3 px-2 ${
           active == index ? "bg-purple text-white" : "bg-trans text-dark"
         } border-0 br-8 py-1`}
--- src/components/SmallComponents/TabsList.tsx
+++ src/components/SmallComponents/TabsList.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import TabButton from "./TabButton";
+
 export default function TabsList() {
   const [active, setActive] = useState(1);
   const onChangeTab = (value: number) => {
@@ -8,31 +9,31 @@ export default function TabsList() {
   return (
     <div className="row mx-auto my-3 d-blue align-items-center bg-white br-8 p-2">
       <TabButton
-        onChangeTab={onChangeTab}
+        onChangeTab={() => onChangeTab(1)}
         index={1}
         active={active}
         title={"Active listings"}
       />
       <TabButton
-        onChangeTab={onChangeTab}
+        onChangeTab={() => onChangeTab(2)}
         index={2}
         active={active}
         title={"Pending listings"}
       />
       <TabButton
-        onChangeTab={onChangeTab}
+        onChangeTab={() => onChangeTab(3)}
         index={3}
         active={active}
         title={"Active listings"}
       />
       <TabButton
-        onChangeTab={onChangeTab}
+        onChangeTab={() => onChangeTab(4)}
         index={4}
         active={active}
         title={"Terminated listings"}
       />
       <TabButton
-        onChangeTab={onChangeTab}
+        onChangeTab={() => onChangeTab(5)}
         index={5}
         active={active}
         title={"Imported listings"}
